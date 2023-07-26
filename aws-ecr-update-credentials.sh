d31e326c4d9803e3d04fba83bdd0edeb8f2c5675
Prem Kumar S <75419846+Prem-Kumar16@users.noreply.github.com>
2023-07-26 14:31:36 +0530

    Update aws-ecr-update-credentials.sh

--- aws-ecr-update-credentials.sh
+++ aws-ecr-update-credentials.sh
@@ -1,8 +1,9 @@
 sudo /usr/local/bin/kubectl delete pod canapp
 sudo /usr/local/bin/kubectl delete service canapp-app-service
-aws configure set aws_access_key_id $(aws --profile default configure get aws_access_key_id)
-aws configure set aws_secret_access_key $(aws --profile default configure get aws_secret_access_key)
-aws configure set default.region ap-south-1
+aws_access_key_id=$(aws --profile default configure get aws_access_key_id)
+aws_secret_access_key=$(aws --profile default configure get aws_secret_access_key)
+aws configure set aws_access_key_id "$aws_access_key_id"
+aws configure set aws_secret_access_key "$aws_secret_access_key"
 sudo /usr/local/bin/kubectl delete secret regcred
 sudo /usr/local/bin/kubectl create secret docker-registry regcred \
   --docker-server=783584839454.dkr.ecr.ap-south-1.amazonaws.com \
